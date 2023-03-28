a3b32f8dbb273a8a1115ec8d564f395a855e816b
Eraly <81900112+Chambo015@users.noreply.github.com>
2023-03-28 23:09:23 +0600

    fix 1.1

--- src/components/SectionAboutCollection.vue
+++ src/components/SectionAboutCollection.vue
@@ -1,23 +1,58 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <section class="min-h-screen bg-[url('/bg-about-collection.png')] bg-no-repeat bg-[length:100%_auto] flex items-center" >
-        <div class="container flex justify-end">
-            <div style="clip-path: url(#my-clip-path-about)"
-        class="w-[930px] h-[767px] z-40 bg-white/5 backdrop-blur-2xl flex justify-center items-center drop-shadow-[0_0_0_10px_#424242] border"></div>
-        </div>
-    </section>
+  <section
+    class="min-h-screen bg-[url('/bg-about-collection.png')] bg-no-repeat bg-[length:100%_auto] flex items-center"
+  >
+    <div class="container flex justify-end">
+      <div
+        class="w-[930px] h-[767px] z-40 flex justify-center items-center relative bg-white/5 backdrop-blur-md"
+        style="clip-path: url(#era)"
+      >
+        <svg
+          width="930"
+          height="767"
+          viewBox="0 0 930 767"
+          xmlns="http://www.w3.org/2000/svg"
+          class="absolute inset-0 w-full h-full"
+        >
+          <path
+            d="M10 0.5H227.543C230.51 0.5 233.307 1.88658 235.104 4.24831L252.351 26.9207C254.336 29.531 257.428 31.0636 260.707 31.0636H920C925.247 31.0636 929.5 35.3169 929.5 40.5636V757C929.5 762.247 925.247 766.5 920 766.5H702.311C699.422 766.5 696.691 765.186 694.888 762.929L677.643 741.339C675.651 738.844 672.632 737.392 669.439 737.392H10C4.7533 737.392 0.5 733.139 0.5 727.892V10C0.5 4.7533 4.7533 0.5 10 0.5Z"
+            stroke="url(#paint0_linear_264_240)"
+            stroke-opacity="0.56"
+            fill="null"
+            fill-opacity=".05"
+          />
 
-  <svg width="0" height="0" viewBox="0 0 930 767" fill="none" xmlns="http://www.w3.org/2000/svg">
-<clipPath id="my-clip-path-about"><path d="M10 0.5H227.543C230.51 0.5 233.307 1.88658 235.104 4.24831L252.351 26.9207C254.336 29.531 257.428 31.0636 260.707 31.0636H920C925.247 31.0636 929.5 35.3169 929.5 40.5636V757C929.5 762.247 925.247 766.5 920 766.5H702.311C699.422 766.5 696.691 765.186 694.888 762.929L677.643 741.339C675.651 738.844 672.632 737.392 669.439 737.392H10C4.7533 737.392 0.5 733.139 0.5 727.892V10C0.5 4.7533 4.7533 0.5 10 0.5Z"/></clipPath>
-</svg>
-
+          <defs>
+            <linearGradient
+              id="paint0_linear_264_240"
+              x1="888.535"
+              y1="-6.825e-05"
+              x2="-106.709"
+              y2="459.651"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#B8B8B8" />
+              <stop offset="0.567708" stop-color="white" stop-opacity="0" />
+              <stop offset="1" stop-color="#A6A6A6" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  </section>
+  <svg width="0"  height="0" viewBox="0 0 930 767">
+    <clipPath id="era">
+      <path
+        d="M10 0.5H227.543C230.51 0.5 233.307 1.88658 235.104 4.24831L252.351 26.9207C254.336 29.531 257.428 31.0636 260.707 31.0636H920C925.247 31.0636 929.5 35.3169 929.5 40.5636V757C929.5 762.247 925.247 766.5 920 766.5H702.311C699.422 766.5 696.691 765.186 694.888 762.929L677.643 741.339C675.651 738.844 672.632 737.392 669.439 737.392H10C4.7533 737.392 0.5 733.139 0.5 727.892V10C0.5 4.7533 4.7533 0.5 10 0.5Z"
+        stroke="url(#paint0_linear_264_240)"
+        stroke-opacity="0.56"
+        fill="null"
+        fill-opacity=".05"
+      />
+    </clipPath>
+  </svg>
 </template>
 
-
-
-<style scoped>
-
-</style>
+<style scoped></style>
